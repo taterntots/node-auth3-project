06a7a4886cce17bf44fbbdaf955e4ff25903d8eb
taterntots <matthew.a.weidner@gmail.com>
2020-01-29 15:54:30 -0500

    login and registration now generate tokens. MVP complete!

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const bc = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
 const Users = require('./users-model.js');
 
 // *****************************************
@@ -13,7 +14,9 @@ router.post('/register', (req, res) => {
 
   Users.add(credentials)
     .then(savedUser => {
-      res.status(201).json(savedUser);
+      const token = generateToken(savedUser);
+
+      res.status(201).json({ savedUser, token });
     })
     .catch(error => {
       res.status(500).json(error);
@@ -29,14 +32,29 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bc.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Logged in! Welcome ${user.username}!` });
+        const token = generateToken(user);
+
+        res.status(200).json({ message: `Logged in! Welcome ${user.username}!`, id: `${user.id}`, token }); //attaches token as part of the response
       } else {
-        res.status(401).json({ message: 'Invalid Credentials.' });
+        res.status(401).json({ message: 'Invalid Credentials. You shall not pass!' });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     })
 })
+
+function generateToken(user) {
+  const payload = {
+    userId: user.id, //sub in payload is what the token is about
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d' //check documentation for other options
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
