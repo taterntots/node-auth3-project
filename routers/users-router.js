d208271a08533d6b5d031e34f6c7f92cfd23a9e5
taterntots <matthew.a.weidner@gmail.com>
2020-01-29 14:57:45 -0500

    added endpoint methods without token implimentation

--- routers/users-router.js
+++ routers/users-router.js
@@ -7,7 +7,13 @@ const Users = require('./users-model.js');
 // gets a list of all users in database
 // *****************************************
 router.get('/', (req, res) => {
-
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: 'You shall not pass!' })
+    })
 })
 
 module.exports = router;
